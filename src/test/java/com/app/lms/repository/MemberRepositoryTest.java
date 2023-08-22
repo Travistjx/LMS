e198dbe90ed18fe454499f24c92c45eaf537bf55
Travis Tham <travistjx.proj@gmail.com>
2023-08-22 23:33:35 +0800

    Update 22/09/2023 Added in some unit tests, and changed some variable names (user -> member)

---
+++ src/test/java/com/app/lms/repository/MemberRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.app.lms.repository;
+
+import com.app.lms.entity.Member;
+import com.app.lms.entity.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository underTest;
+
+    @Test
+    void itShouldCheckIfUserExistsByEmail() {
+
+        //given
+        Member member = new Member();
+        member.setEmail("georgeng@gmail.com");
+
+        member.setFirstName("george");
+        member.setLastName("ng");
+        member.setPassword("123");
+
+        Role role = new Role();
+        role.setName("ROLE_MEMBER");
+        member.setRoles(Arrays.asList(role));
+        underTest.save(member);
+
+        //when
+        boolean exists = false;
+        Member newMember = underTest.findByEmail("georgeng@gmail.com");
+        if (newMember != null) exists = true;
+
+        //then
+        assertThat(exists).isTrue();
+    }
+}
